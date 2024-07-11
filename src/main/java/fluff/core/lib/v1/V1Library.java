0771e608bed12e41c20bcca5bce57e90ed4e4e99
Musca Mihail <muscamihaill@gmail.com>
2024-07-11 21:02:59 +0300

    Javadoc & fluff_lib.info changes

--- src/main/java/fluff/core/lib/v1/V1Library.java
+++ src/main/java/fluff/core/lib/v1/V1Library.java
@@ -8,6 +8,9 @@ import fluff.core.lib.ILibrary;
 import fluff.core.lib.LibraryException;
 import fluff.core.lib.info.LibraryInfoReader;
 
+/**
+ * Represents a version 1 library implementation of the the {@link ILibrary} interface.
+ */
 public class V1Library implements ILibrary {
     
     private final String author;
@@ -16,32 +19,38 @@ public class V1Library implements ILibrary {
     private final String url;
     private final String mainClass;
     
+    /**
+     * Constructs a V1Library object using the information provided by the {@link LibraryInfoReader}.
+     * 
+     * @param r the {@link LibraryInfoReader} instance to read library information from
+     * @throws LibraryException if there is an error while reading or processing library information
+     */
     public V1Library(LibraryInfoReader r) throws LibraryException {
         author = r.required("author", "Property missing: author")
-                	.String();
+                    .String();
         
         id = r.required("id", "Property missing: id")
-                	.String();
+                 .String();
         
         dependencies = r.optional("depends")
-	                .transform(Set.class)
-	                .If(Objects::nonNull, v -> {
-	                    Set<String> set = new HashSet<>();
-	                    for (String tag : v.split(",")) {
-	                    	tag = tag.trim();
-	                        set.add(tag);
-	                    }
-	                    return set;
-	                })
-	                .Else(v -> new HashSet<>())
-	                .Result();
+                        .transform(Set.class)
+                        .If(Objects::nonNull, v -> {
+                            Set<String> set = new HashSet<>();
+                            for (String tag : v.split(",")) {
+                                tag = tag.trim();
+                                set.add(tag);
+                            }
+                            return set;
+                        })
+                        .Else(v -> new HashSet<>())
+                        .Result();
         if (!author.equals("muscaa") || !id.equals("fluff-core")) dependencies.add("muscaa/fluff-core");
         
         url = r.optional("url")
-        			.String();
+                    .String();
         
         mainClass = r.optional("class")
-	                .String();
+                        .String();
     }
     
     @Override
--- src/main/java/fluff/core/lib/v1/V1LibraryManager.java
+++ src/main/java/fluff/core/lib/v1/V1LibraryManager.java
@@ -8,10 +8,13 @@ import fluff.core.lib.ILibraryManager;
 import fluff.core.lib.LibraryException;
 import fluff.core.lib.info.LibraryInfoReader;
 
+/**
+ * Implements {@link ILibraryManager} for managing v1 libraries.
+ */
 public class V1LibraryManager implements ILibraryManager<V1Library> {
-	
+    
     public static final String ID = "v1";
-	
+    
     @Override
     public V1Library create(BufferedReader reader) throws LibraryException {
         return new V1Library(new LibraryInfoReader(reader));
@@ -19,29 +22,29 @@ public class V1LibraryManager implements ILibraryManager<V1Library> {
     
     @Override
     public void load(ClassLoader loader, V1Library library) throws LibraryException {
-    	if (library.getMainClass() == null) return;
-    	if (library.getMainClass().equals("fluff.core.FluffCore")) return;
-    	
-		try {
-			Class<?> clazz = loader.loadClass(library.getMainClass());
-			
-	    	for (Method m : clazz.getDeclaredMethods()) {
-	            if (!m.isAnnotationPresent(LibraryMain.class)) continue;
-	            if (!Modifier.isStatic(m.getModifiers())) continue;
-	            
+        if (library.getMainClass() == null) return;
+        if (library.getMainClass().equals("fluff.core.FluffCore")) return;
+        
+        try {
+            Class<?> clazz = loader.loadClass(library.getMainClass());
+            
+            for (Method m : clazz.getDeclaredMethods()) {
+                if (!m.isAnnotationPresent(LibraryMain.class)) continue;
+                if (!Modifier.isStatic(m.getModifiers())) continue;
+                
                 m.setAccessible(true);
                 m.invoke(null);
-	        }
-		} catch (Exception e) {
-			throw new LibraryException(e);
-		}
+            }
+        } catch (Exception e) {
+            throw new LibraryException(e);
+        }
     }
-	
+    
     @Override
     public String getTag(V1Library lib) {
         return lib.getAuthor() + "/" + lib.getID();
     }
-	
+    
     @Override
     public String getID() {
         return ID;
